fb65d7ba9dd411339df4faf305e38312ba284648
Simona <see.moona@yahoo.com>
2020-02-21 15:52:02 +0100

    creating objects+methods

--- objectsBasics.js
+++ objectsBasics.js
@@ -23,21 +23,28 @@ student2.print()
 
 // Create a person object. Include the person's first and last name, age, job, city etc. Create a method to print data 
 // from the object e.g. "John Smith is a 41 year old engineer living in France".
-const person = { 
-    firstName: "John", 
-    lastName: "Smith", 
-    age: 32,
-    job: "teacher",
-    city: "Berlin",
-    print(){
-        console.log(`${this.firstName} ${this.lastName} is a ${this.age} old ${this.job} living in ${this.city}`)
-    } 
-    
-};
-person.print();
+const person = {
+    firstName: "John",
+    lastName: "Smith",
+    job: "engineer",
+    age: 46,
+    country: "france",
+    personPrinter(){
+        console.log(`${this.firstName} ${this.lastName} is a ${this.age} old ${this.job} living in ${this.country}.`);
+    },
+    getLength() {
+        let length = 0;
+        for(key in this){
+            length++;
+        }
+        console.log(length);
+    }
+}
+person.personPrinter();
+person.getLength();
 
 // Bonus
 // Write a method to get the length of the person object.
 
-let length = Object.keys(person).length;
-console.log(length)
+// let length = Object.keys(person).length;
+// console.log(length)
